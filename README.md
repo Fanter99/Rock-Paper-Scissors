97d27ca7637122c6b83315b7a98d635b69125b01
Egor <egor.dmitriev99@gmail.com>
2023-04-05 23:42:52 +0200

    1. Phase is done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Rock-Paper-Scissors Assignment for the Odin Project.
 
 will made in 2 Phases:
-    1. Basic Javasript logic (console variant) {In Progress}
-    2. UI design
+    1. Basic Javasript logic (console variant) 
+    2. UI design {In Progress}
--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const Figures = {
 const Figures_Array = Object.keys(Figures)
 
 function getComputerChoice(){
-    console.log(Figures_Array[Math.floor(Math.random() * 3)])
+    return Figures_Array[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
 
-    const tuple = [playerSelection, computerSelection]    
+    const tuple = [playerSelection, computerSelection]
+    // console.log(tuple)    
     switch (JSON.stringify(tuple)){
         case JSON.stringify(["Rock", "Rock"]):
             return "Draw";
@@ -24,11 +25,11 @@ function playRound(playerSelection, computerSelection) {
             break;
 
         case JSON.stringify(["Rock", "Scissors"]):
-            return "Win!";
+            return "Win";
             break;
 
         case JSON.stringify(["Paper", "Rock"]):
-            return "Win!";
+            return "Win";
             break;
 
         case JSON.stringify(["Paper", "Paper"]):
@@ -40,7 +41,7 @@ function playRound(playerSelection, computerSelection) {
             break;
         
         case JSON.stringify(["Scissors", "Rock"]):
-            return "Lose!";
+            return "Lose";
             break;
     
         case JSON.stringify(["Scissors", "Paper"]):
@@ -59,5 +60,43 @@ function playRound(playerSelection, computerSelection) {
 
   }
    
+function game() {
+    let count = [0,0]
+    for (let i = 0; i<5 ; i++) {
+        let UserChoise = prompt("Your figure?")
+        UserChoise = UserChoise.toLowerCase()
+        
+        UserChoise = UserChoise.substring(0,1).toUpperCase() + UserChoise.substring(1)
+        console.log(UserChoise)
+        let ComputerChoice = getComputerChoice()
+        // console.log(UserChoise, ComputerChoice)
 
-console.log(playRound("Paper", "Scissors"));
+        let Round = playRound(UserChoise, ComputerChoice)
+        
+        switch (Round) {
+            case "Win":
+                console.log(`You've won the ${i+1} round`)
+                count[0] += 1
+                break;
+            case "Lose":
+                console.log(`You've lost the ${i+1} round`)
+                count[1] += 1
+                break;
+            default:
+                console.log(`it was a draw.`)    
+
+        }
+        console.log(`Current count is ${count[0]}:${count[1]}`)
+    }
+    if (count[0] > count[1]) {
+        console.log("You've won the game!!!")
+    }
+    else if (count[0] < count[1]){
+        console.log("You've lost the game!!!")        
+    }
+    else {
+        console.log("It is a draw!!!")
+    }
+}  
+
+game()
